6aa145a4bf8cb2f26591be3249a83e4260dce2c8
max <Max1mus1995.ms@gmail.com>
2022-11-21 18:09:41 +0300

    start work weather

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const API = "https://goweather.herokuapp.com/weather/";
-
   let place = "Astana";
   let htmlPlace = document.querySelector(".location__place");
   let htmlTemperature = document.querySelector(".temperature");
@@ -10,37 +8,48 @@ document.addEventListener("DOMContentLoaded", function () {
   let htmlInput = htmlForm.querySelector("input[type='text']");
   let htmlImgWeather = document.querySelector(".weatherIMG");
 
-  getWeather();
+  const API = `http://api.openweathermap.org/geo/1.0/direct?limit=5&appid=6e1d8dc51c469cac8afd31a78b90ebd9&q=`;
+  const API2 = `https://api.openweathermap.org/data/2.5/weather?appid=6e1d8dc51c469cac8afd31a78b90ebd9&lat=`;
+
+  getWeatherByCoordinats();
 
   htmlForm.addEventListener("submit", function (ev) {
     ev.preventDefault();
 
     place = htmlInput.value;
 
-    getWeather();
+    getWeatherByCoordinats();
   });
 
-  function getWeather() {
-    fetch(API + place)
-      .then((response) => {
-        return response.json();
-      })
-      .then((weather) => {
-        showWeather(weather);
+  async function getCoordinats(place) {
+    const locationByCoord = await fetch(API + place)
+    .then(response => response.json());
 
-      });
+    return locationByCoord;
+  }
+
+  async function getWeatherByCoordinats(i = 0) {
+    const coordinates = await getCoordinats(place);
+
+    let lat = coordinates[i].lat;
+    let lon = coordinates[i].lon;
+
+    await fetch(API2 +`${lat}&lon=${lon}`)
+    .then(response => response.json())
+    .then(data => showWeather(data));
   }
 
   function showWeather(data) {
     htmlPlace.innerText = place;
-    htmlTemperature.innerText = data.temperature;
-    htmlWind.innerText = data.wind;
-    htmlDescription.innerText = data.description;
-    htmlImgWeather.src = getIconByDescription(data.description);
+    htmlTemperature.innerText = (data.main.temp - 273).toFixed(1) + "°C";
+    htmlWind.innerText = data.wind.speed + " m/s";
+    htmlDescription.innerText = data.weather[0].description;
+    htmlImgWeather.src = getIconByDescription(data.weather[0].description);
+
 
   }
 
-  function getIconByDescription (description) {
+  function getIconByDescription(description) {
     const path = "/img/";
     const Mapping = {
       "Light snow": "snowy-1.svg",
@@ -48,12 +57,14 @@ document.addEventListener("DOMContentLoaded", function () {
       "Light rain": "rainy-1.svg",
       "Clear": "clear-night.svg",
       "Sunny": "clear-day.svg",
-    }
+      "overcast clouds": "cloudy.svg",
+      "haze": "haze.svg",
+      "clear sky": "clear-day.svg",
+      "broken clouds": "cloudy-1-day.svg",
+    };
 
     description = description.split(",")[0];
 
     return path + Mapping[description];
   }
-
-
 });
